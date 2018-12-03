6ed263fb7e234ba544f7df6aef9e788df2bdd70f
Frank van Wijk <fvanwijk@xebia.com>
2018-12-04 00:05:14 +0100

    Solution for Day 3 part 2

--- 2018/3.js
+++ 2018/3.js
@@ -2,27 +2,47 @@ const h = require('../helpers');
 const f = h.getFileSync('3');
 
 const input = f.split('\n');
-const res = input.reduce((mem, l) => {
-  const [
-    match,
-    id,
-    left,
-    top,
-    width,
-    height
-  ] = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/g.exec(l);
+const res = input.reduce(
+  ({ count, ids }, l) => {
+    const [
+      match,
+      id,
+      left,
+      top,
+      width,
+      height
+    ] = /#(\d+) @ (\d+),(\d+): (\d+)x(\d+)/g.exec(l);
 
-  for (let i = +left; i < +left + +width; i++) {
-    for (let j = +top; j < +top + +height; j++) {
-      let key = `${i}-${j}`;
+    for (let i = +left; i < +left + +width; i++) {
+      for (let j = +top; j < +top + +height; j++) {
+        let key = `${i}-${j}`;
 
-      h.increment(mem, key, 1);
+        if (count[key]) {
+          count[key].c++;
+        } else {
+          count[key] = { id, key, c: 1 };
+        }
+      }
     }
-  }
 
-  return mem;
-}, {});
+    ids[id] = +width * +height;
 
-const countOverlaps = Object.values(res).filter(d => d > 1).length;
+    return { count, ids };
+  },
+  { count: {}, ids: {} }
+);
+
+const counts = Object.values(res.count);
+const countOverlaps = counts.filter(d => d.c > 1).length;
+const noOverlapCountPerId = counts.reduce(
+  (mem, d) => h.increment(mem, d.id, d.c === 1 ? 1 : 0),
+  {}
+);
 
 console.log('Part 1:', countOverlaps);
+
+for (key in noOverlapCountPerId) {
+  if (noOverlapCountPerId[key] === res.ids[key]) {
+    console.log('Part 2', key);
+  }
+}
